439a76d57826f06f1b89257bcb28eb21a8783fee
Deon Slabbert <dmslabbert@gmail.com>
2024-05-13 09:30:44 +1000

    updated home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Becoming a Programmer",
-  tagline: "one step at a time...",
+  tagline: "making sense of the world of programming",
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
