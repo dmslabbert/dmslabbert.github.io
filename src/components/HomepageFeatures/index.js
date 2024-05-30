a5ac6304e1949e6b7c52aa090eb3916eb7d4d53b
Deon Slabbert <dmslabbert@gmail.com>
2024-05-30 15:57:21 +1000

    updated index image

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,108 +1,25 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-
-
-
-const FeatureList = [
-  {
-    image: '/img/pexels-realtoughcandy-11035547.jpg',
-  },
-  {
-    image: '/img/pexels-realtoughcandy-11035473.jpg',
-  },
-  {
-    image: '/img/pexels-realtoughcandy-11035396.jpg',
-  },
-  {
-    image: '/img/pexels-realtoughcandy-11035465.jpg',
-  },
-  {
-    image: '/img/pexels-realtoughcandy-11035539.jpg',
-  },
-  {
-    image: '/img/pexels-realtoughcandy-11035540.jpg',
-  },
-  // {
-  //   title: 'Focus on What Matters',
-  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-  //   description: (
-  //     <>
-  //       Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-  //       ahead and move your docs into the <code>docs</code> directory.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-  // {
-  //   title: 'Powered by React',
-  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-  //   description: (
-  //     <>
-  //       Extend or customize your website layout by reusing React. Docusaurus can
-  //       be extended while reusing the same header and footer.
-  //     </>
-  //   ),
-  // },
-];
-
-function Feature({title, image, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img src={image} />
-      </div>
-      {/* <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div> */}
-    </div>
-  );
-}
+import React from "react"
+import styles from "./styles.module.css"
+import useBaseUrl from "@docusaurus/useBaseUrl"
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {/* <div>
-            <img className='index-page-image-center' src={useBaseUrl('/img/pexels-realtoughcandy-11035547.jpg')} />
-          </div> */}
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    <div>
+                        <img
+                            className="index-page-image-center"
+                            src={useBaseUrl("/img/4136918.jpg")}
+                        />
+                        <div className="attribution">
+                            <a href="https://www.freepik.com/free-vector/developer-activity-concept-illustration_9319773.htm#fromView=search&page=1&position=2&uuid=46bd2fa2-105e-486b-9859-461e4e57fe6e">
+                                Image by storyset on Freepik
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
