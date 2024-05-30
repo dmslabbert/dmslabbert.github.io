f39a815efe756f90e7fd8992822d68cd8a7f2e10
Deon Slabbert <dmslabbert@gmail.com>
2024-05-31 09:58:45 +1000

    updated sub title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Programming in Practice',
-  tagline: "making sense of the world of programming...",
+  tagline: "making sense of programming...",
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
